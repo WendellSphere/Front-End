78515a7afca05e7a4a80b9766e925b70a6098e0f
WendellSphere <WendellSphere@users.noreply.github.com>
2017-06-10 21:41:06 -0700

    Add files via upload

---
+++ productService.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    "use strict";
+
+    angular.module(APPNAME).factory('$productService', ProductService);
+
+    ProductService.$inject = ['$baseService', '$sabio', '$http'];
+
+    function ProductService($baseService, $sabio, $http) {
+        var productServiceObj =  {
+            getProducts: _getProducts
+            ,get3Products: _get3Products
+            , getAddOns: _getAddOns
+        };
+
+        var newService = $baseService.merge(true, {}, productServiceObj, $baseService);
+
+        return newService;
+
+        function _get3Products(onSuccess, onError) {
+            $http({
+                method: "GET",
+                url: "/api/products/addons"
+            }).then(onSuccess
+                , onError);
+        }
+        function _getAddOns(id, onSuccess, onError) {
+            
+            $http({
+                method: "GET",
+                url: "/api/products/addons/" + id
+            }).then(onSuccess
+                , onError);          
+        }
+
+        function _getProducts(onSuccess,onError) {
+            $http({
+                method: "GET",
+                url: "/api/products/getall"
+            }).then(onSuccess
+               , onError);
+        }
+
+
+    }
+})();
